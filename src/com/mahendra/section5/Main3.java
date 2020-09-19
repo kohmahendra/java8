49bebf9d5c0161729f8e8cae63b189167740b1d0
Mahendra Reddy Pappu <kohmahendra@gmail.com>
2020-09-18 23:54:46 -0400

    changes

--- src/com/mahendra/section5/Main3.java
+++ src/com/mahendra/section5/Main3.java
@@ -1,7 +1,13 @@
 package com.mahendra.section5;
 
-public interface IntOne {
+public class Main3 implements IntOne {
+
+	public static void main(String[] args) {
+		m1();
+	}
+
 	public static void m1() {
 		System.out.println("IntOne.m1()");
 	}
+
 }
